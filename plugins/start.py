678d59527e5b149d5c1389eafbea37d57b35a26e
sanuksd <100213849+sanuksd@users.noreply.github.com>
2022-05-31 19:34:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,10 +12,10 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup([[
-          InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/Mo_Tech_Group"), 
-	  InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/MoTech_YT")
+          InlineKeyboardButton("Support" ,url="https://t.me/sanusmovies"), 
+	  InlineKeyboardButton("Subscribe", url="https://t.me/+YX8I4WjAjrkxNmRh")
           ],[
-          InlineKeyboardButton("🧩 Deploy 🧩", url="https://youtu.be/7ALYUk-rYMc")
+          InlineKeyboardButton("owner", url="https://t.me/sanumovieowner")
           ]]
           )
         )
